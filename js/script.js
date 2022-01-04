569c5ac7270b75a887c94cdc9b86aa595b5f17aa
Danilo Baraldi <danilobaraldi@hotmail.com>
2022-01-04 19:06:55 -0300

    fixed call to innerHTML, project completed, now work with some improvements at logic code

--- js/script.js
+++ js/script.js
@@ -13,44 +13,42 @@ let resultado = document.getElementById("resultado");
 
 function calcular(){
 
-    let adultos = inputAdultos.ariaValueMax;
+    let adultos = inputAdultos.value;
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
 
-    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) * criancas);
+    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
     let qtdTotalCerveja = cervejaPP(duracao) * adultos;
-    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) * criancas);
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
 
-    resultado.innerHTML = '<p>${qtdTotalCarne}g de Carne</p>';
-    resultado.innerHTML += '<p>${qtdTotalCerveja}ml de Cerveja</p>';
-    resultado.innerHTML += '<p>${qtdTotalBebidas}ml de Bebidas</p>';
+
+    resultado.innerHTML = `<p>${qtdTotalCarne / 1000} Kg de Carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 750)} garrafas de Cerveja</p>`;
+    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas / 2000)} garravas de Bebidas</p>`;
 
 }
-//Calcula quantidade de carner por pessoa
-function carnePP(duracao){
-    if (duracao >= 6){
+function carnePP(duracao) {
+    if (duracao >= 6) {
         return 650;
     } else {
         return 400;
     }
-    
 }
-//calcular quantidade de cerveja POR ADULTO! 
-function cervejaPP(duracao){
-    if (duracao >= 6){
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
         return 2000;
     } else {
         return 1200;
     }
-    
 }
-
-//calcular quantidade de bebidas por pessoa! 
-function bebidasPP(duracao){
-    if (duracao >= 6){
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
         return 1500;
     } else {
-        return 1200;
+        return 1000;
     }
-    
 }
+
+
+console.log("acabei")
