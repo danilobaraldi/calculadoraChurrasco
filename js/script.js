51c080f0bcc319c4ee7c9fdb4214dd37f74c1e09
Danilo Baraldi <danilobaraldi@hotmail.com>
2022-01-04 18:17:33 -0300

    defined layout

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Qual a quantidade de comida e bebida em relação ao tempo do churrasco? 
+//Carne - 400 gramas por pessoa mais de 6 horas = 650
+//Cerveja - 1200ml por pessoa mais de 6 horas = 2000ml
+//Refrigerante/agua - 1000ml por pessoa mais de 6 horas = 1500ml
+// Crianças contam como meia pessoa no consumo. 
